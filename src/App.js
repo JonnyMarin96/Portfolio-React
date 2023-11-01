a29d5d5a4680b8026bcede138a33fe30958679bb
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-10-31 20:53:24 -0600

    Se agrego archivo para iniciar componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from './Components/Menu/Menu';
 
 function App() {
   return (
-    <></>
-  );
+    <Menu/>
+  )
 }
 
 export default App;
---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,8 @@
+import "./menu.css"
+
+const Menu = () => {
+    return <h1>ujerkj</h1>
+}
+
+
+export default Menu
