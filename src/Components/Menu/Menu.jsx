fb38eac7b61549957cf39c24d9ea9f83050b6002
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-11-01 19:35:36 -0600

    Se agrego reset css

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,7 +1,20 @@
 import "./menu.css"
 
+import MenuBoton from "../MenuBoton/MenuBoton"
+
 const Menu = () => {
-    return <h1>ujerkj</h1>
+    return <>
+    <div className="menuContainer">
+        <ul className="Menu">
+            <MenuBoton titulo="Inicio" />
+            <MenuBoton titulo="Sobre mí" />
+            <MenuBoton titulo="Educacion" />
+            <MenuBoton titulo="Skills" />
+            <MenuBoton titulo="Contacto" />
+        </ul>
+    </div>
+    
+    </>
 }
 
 
