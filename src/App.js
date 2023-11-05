b434897edc0ee7bbd1a6475f08be8882e5c98ea8
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-11-05 01:37:36 -0600

    Esqueleto seccion Sobre Mi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import './reset.css'
 import Menu from './Components/Menu/Menu';
+import SobreMi from './Components/SobreMi/SobreMi';
 
 function App() {
-  return (
+  return (<>
     <Menu/>
-  )
+    <SobreMi/>
+  </>)
 }
 
 export default App
---
+++ src/Components/SobreMi/SobreMi.jsx
@@ -0,0 +1,20 @@
+import './SobreMi.css'
+
+const SobreMi = () => {
+    return<>
+        <section className='ContainerSobreMi'>
+            <label>Jonathan Marin Marin</label>
+            <label htmlFor="">Desarrollador Front-end Jr</label>
+            <p> Presentacion </p>
+            <ul>
+                <li>Linked In</li>
+                <li>Github</li>
+                <li>Instagram</li>
+                <li>Tiktok</li>
+            </ul>
+            <button className='btn_CV'>Descargar CV</button>
+        </section>
+    </>
+}
+
+export default SobreMi
