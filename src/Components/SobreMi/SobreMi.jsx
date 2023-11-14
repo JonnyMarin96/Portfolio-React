93196b295de3eb1e94dcf2da3f00716c6914dda2
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-11-13 19:22:08 -0600

    Base "Sober Mi"

--- src/Components/SobreMi/SobreMi.jsx
+++ src/Components/SobreMi/SobreMi.jsx
@@ -1,8 +1,18 @@
 import './SobreMi.css'
+import ProfileImg from './profileImg.png'
 
 const SobreMi = () => {
     return (
-        <div className='asd'></div>
+
+        <section className='SobreMiContainer'>
+            <img src={ProfileImg} alt="Foto Personal" className='FotoPersonal'/>
+            <p className='Descripcion'>
+                Mi misión es convertir ideas creativas en experiencias interactivas y atractivas.
+                Estoy emocionado por seguir ampliando mis conocimientos y colaborar en proyectos desafiantes que me permitan
+                exprimir al máximo mi creatividad y habilidades técnicas. ¡Siempre estoy listo para enfrentar nuevos retos
+                y construir experiencias digitales asombrosas
+            </p>
+        </section>
     )
 }
 
