bb403a28f711c176aa562544559513106edc3ef4
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-11-14 19:09:29 -0600

    provando modificaciones

--- src/Components/SobreMi/SobreMi.jsx
+++ src/Components/SobreMi/SobreMi.jsx
@@ -5,13 +5,15 @@ const SobreMi = () => {
     return (
 
         <section className='SobreMiContainer'>
-            <img src={ProfileImg} alt="Foto Personal" className='FotoPersonal'/>
-            <p className='Descripcion'>
-                Mi misión es convertir ideas creativas en experiencias interactivas y atractivas.
-                Estoy emocionado por seguir ampliando mis conocimientos y colaborar en proyectos desafiantes que me permitan
-                exprimir al máximo mi creatividad y habilidades técnicas. ¡Siempre estoy listo para enfrentar nuevos retos
-                y construir experiencias digitales asombrosas
-            </p>
+            <div>
+                <img src={ProfileImg} alt="Foto Personal" className='FotoPersonal'/>
+                <p className='Descripcion'>
+                    Mi misión es convertir ideas creativas en experiencias interactivas y atractivas.
+                    Estoy emocionado por seguir ampliando mis conocimientos y colaborar en proyectos desafiantes que me permitan
+                    exprimir al máximo mi creatividad y habilidades técnicas. ¡Siempre estoy listo para enfrentar nuevos retos
+                    y construir experiencias digitales asombrosas
+                </p>
+            </div>
         </section>
     )
 }
