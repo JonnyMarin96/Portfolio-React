9862b9d3f7aa2e30ed3a0ed59f5739f5089bbcfe
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-11-20 17:33:53 -0600

    Se agrego formulario

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -1,13 +1,20 @@
 import './Contacto.css'
-import { TextField } from '@mui/material'
+import RedesSociales from '../RedesSociales/RedesSociales'
+import { Box, TextField } from '@mui/material'
+import { Block } from '@mui/icons-material'
+
 
 const Contacto = () => {
     return<>
         <section className='ContactoContainer'>
-            <TextField variant="filled" />
-            <TextField variant="filled" />
-            <TextField variant="filled" />
-            <TextField variant="filled" />
+            <label className='Etiqueta'>Contactame!</label>
+            <RedesSociales/>
+            <form className='FormContacto'>
+                <TextField variant="filled" className='Contacto' label='Nombre'/>
+                <TextField variant="filled" className='Contacto' label='Correo'/>
+                <TextField variant="filled" className='Contacto' label='Asunto'/>
+                <TextField variant="filled" className='Contacto' label='Mensaje'/>
+            </form>
         </section>
     </> 
 }
