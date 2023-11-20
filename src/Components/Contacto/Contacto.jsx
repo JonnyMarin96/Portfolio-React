429ce2370f4ef3e9279318174671c08f3725f15b
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-11-19 19:52:38 -0600

    Se agregaron textfields

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -1,9 +1,13 @@
 import './Contacto.css'
+import { TextField } from '@mui/material'
 
 const Contacto = () => {
     return<>
         <section className='ContactoContainer'>
-
+            <TextField variant="filled" />
+            <TextField variant="filled" />
+            <TextField variant="filled" />
+            <TextField variant="filled" />
         </section>
     </> 
 }
