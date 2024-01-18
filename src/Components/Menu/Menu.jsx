3bb6aceee7093b0951df0881e304888e01e15a77
JonnyMarin96 <jonny.yair.2@gmail.com>
2024-01-18 16:35:24 -0600

    Se modifico menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -10,16 +10,12 @@ const Menu = () => {
     <div className="menuContainer">
 
         <ul className="Menu">
-            <img src={icono} alt="avatar" className="avatarImg"/>
             <MenuBoton titulo="INICIO" />
             <MenuBoton titulo="SOBRE MI" />
             <MenuBoton titulo="PROYECTOS" />
             <MenuBoton titulo="SKILLS" />
             <MenuBoton titulo="EDUCACIÓN" />
             <MenuBoton titulo="CONTACTO" />
-            <div className="Redes">
-                <RedesSociales/>
-            </div>
         </ul>
     </div>
     
