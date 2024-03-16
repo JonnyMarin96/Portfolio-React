644560d39906d9a64b56bf1be8ef355f4cca5b1c
JonnyMarin96 <jonny.yair.2@gmail.com>
2024-03-15 20:14:23 -0600

    Save

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -5,7 +5,12 @@ const Skills = () => {
     return(<>
         
         <section className="SkillsContainer">
+            <label htmlFor="">Habilidades</label>
 
+            <section className="HardSkills"> Hard Skills </section>
+            <section className="HardSkills"> Soft skills </section>
+            <section className="HardSkills"> Librerias </section>
+            <section className="HardSkills"> Habilidades varias </section>
         </section>
         
     </>)
